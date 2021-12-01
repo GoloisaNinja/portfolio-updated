11840dd2f4e8a8bf1b3e3cad4411c058493fd0ef
Golodhrim <holidayninjastaff@gmail.com>
2021-12-01 01:40:27 -0500

    added if statement to node file to check for prod and not include source maps if prod env is true to hopefully bump performance

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,11 @@
 const path = require("path");
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  if (getConfig().mode === "production") {
+    actions.setWebpackConfig({
+      devtool: false,
+    });
+  }
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, "src"), "node_modules"],
